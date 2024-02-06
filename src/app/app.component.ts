0a34e40909c09a0ae20c6a2df098a9f3e2679a7d
softwareCantrips <softwarecantrips@gmail.com>
2024-02-06 19:40:31 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import {MatDrawerMode, MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import { BooleanInput } from '@angular/cdk/coercion';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,
+    MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'soca13';
+  mdcBackdrop: BooleanInput = false;
+  drawerMode: MatDrawerMode = "push";
+
+  readonly breakpoint$ = this.breakpointObserver
+  .observe([ '(max-width: 500px)']);
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpoint$.subscribe(() =>
+      this.breakpointChanges()
+  );
+  }
+
+  breakpointChanges(): void {
+
+    if (this.breakpointObserver.isMatched('(max-width: 500px)')) {
+      this.drawerMode = "over";
+      this.mdcBackdrop = true;
+    } else {
+      this.drawerMode = "push";
+      this.mdcBackdrop = false;
+    }
+    
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'content', component: ContentComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
